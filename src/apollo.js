1e74c70ba939608f75e849c71221d5abdd4008ef
여정식 <jungsik.yeo@multicampus.com>
2021-03-29 18:28:58 +0900

    전체 강의 학습 완료

--- src/apollo.js
+++ src/apollo.js
@@ -1,7 +1,22 @@
 import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
-    uri: "https://movieql2.now.sh/"
+  uri: "https://movieql2.now.sh/",
+  resolvers: {
+    Movie: {
+      isLiked: () => false,
+    },
+    Mutation: {
+      toggleLikeMovie: (_, { id, isLiked }, { cache }) => {
+        cache.writeData({
+          id: `Movie:${id}`,
+          data: {
+            isLiked: !isLiked,
+          },
+        });
+      },
+    },
+  },
 });
 
 export default client;
