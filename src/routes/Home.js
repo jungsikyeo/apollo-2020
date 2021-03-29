5ed787e023126cec8144ab32ad3384855c78cabb
여정식 <jungsik.yeo@multicampus.com>
2021-03-29 16:34:19 +0900

    #1.5 강의 학습 완료

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { gql, useQuery } from "@apollo/client";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
 import styled from "styled-components";
 import Movie from "../components/Movie";
 
@@ -20,20 +21,20 @@ const Container = styled.div`
 `;
 
 const Header = styled.header`
-    background-image: linear-gradient(-45deg, #d754ab, #fd723a);
-    height: 45vh;
-    color: white;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
 `;
 
 const Title = styled.h1`
-    font-size: 60px;
-    font-weight: 600;
-    margin-bottom: 20px;
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
 `;
 
 const Subtitle = styled.h3`
@@ -47,6 +48,15 @@ const Loading = styled.div`
   margin-top: 10px;
 `;
 
+const Movies = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 25px;
+  width: 60%;
+  position: relative;
+  top: -50px;
+`;
+
 export default () => {
   const { loading, data } = useQuery(GET_MOVIES);
   return (
@@ -56,7 +66,11 @@ export default () => {
         <Subtitle>I love GraphQL</Subtitle>
       </Header>
       {loading && <Loading>Loading...</Loading>}
-      {!loading && data.movies && data.movies.map((m) => <Movie key={m.id} id={m.id} />)}
+      <Movies>
+        {data?.movies?.map((m) => (
+          <Movie key={m.id} id={m.id} bg={m.medium_cover_image} />
+        ))}
+      </Movies>
     </Container>
   );
 };
